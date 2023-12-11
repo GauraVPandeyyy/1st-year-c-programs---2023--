0f4c16bcdce700c7ea1c8e12a1a1d5bdc1c883b6
GauraVPandeyyy <153275712+GauraVPandeyyy@users.noreply.github.com>
2023-12-11 15:37:02 +0530

    Delete array.c

--- array.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main () {
-float frnt; 
-printf("enter value of farhenhiet  : ");
-scanf("%f",&frnt);
-
-float celsius = (frnt -32) * 5/9;
-printf("CELSIUS IS  :  %f",celsius);
-}
